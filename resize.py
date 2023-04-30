3cecb935f8d8ab737cec13d2b0f02d1263bd432c
Eduardo Nicoleit <33761603+EduardoNicoleit@users.noreply.github.com>
2023-04-30 00:23:49 -0300

    Update resize.py

--- resize.py
+++ resize.py
@@ -13,9 +13,11 @@ extensions = ['jpg','png','jpeg']
 for file in files:
     ext = file.split('.')[-1]
     if ext in extensions:
+        
         im = Image.open('assets/'+file)
         im_resized = resize(im, 600)
         new_dir = 'resized_images'
+        
         if not os.path.exists(new_dir):
             os.makedirs(new_dir)
         file = f'{new_dir}/{file}.jpg'
